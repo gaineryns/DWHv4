a79d2f3e0e1be3ad7b93f4c457c97b932782d076
gaineryns <gaineryns@gmail.com>
2017-03-05 12:53:28 +0100

    api test good :P 222

---
+++ src/providers/product-service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the ProductService provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class ProductService {
+data ="";
+  constructor(public http: Http) {
+    console.log('Hello ProductService Provider');
+  }
+
+  load(){
+    if(this.data){
+      return Promise.resolve(this.data);
+    }
+    return new Promise(resolve => {
+      this.http.get('https://randomuser.me/api/?results=10')
+          .map(res => res.json())
+          .subscribe(data => {
+            this.data = data.results;
+            resolve(this.data);
+          });
+    });
+  }
+
+
+}
